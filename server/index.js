609f5451bcd6e14c75d73c3aa78e7e3b1df6249a
Vicky kumar <148261348+vickytheddev@users.noreply.github.com>
2026-09-10 21:29:33 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://websitebuilder-1-fp79.onrender.com",
     credentials:true
 }))
 app.use("/api/auth",authRouter)
